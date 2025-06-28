5465074196aa7be6deb1ff3650213e26651a0dcf
Mikita Rudnik <mikita.rudnik@gmail.com>
2025-06-29 00:50:31 +0200

    feat: add theming

--- src/app/pages/styles-laboratory/styles-laboratory.component.ts
+++ src/app/pages/styles-laboratory/styles-laboratory.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 import { StylesLabPath } from '@/shared/constants/app.config';
 
 @Component({
   selector: 'app-styles-laboratory',
   templateUrl: './styles-laboratory.component.html',
-  styleUrl: './styles-laboratory.component.scss'
+  styleUrl: './styles-laboratory.component.scss',
+  encapsulation: ViewEncapsulation.None
 })
 export class StylesLaboratoryComponent {
   StylesLabPath = StylesLabPath;
